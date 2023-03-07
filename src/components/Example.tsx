ec7a08d3ae3a8a9b9cf18fb56eb9a1d502c3a77f
esolodovnik <elizavetasolodovnik12@mail.ru>
2023-03-07 21:19:13 +0300

    first commit

---
+++ src/components/Example.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Example.module.scss'
+
+const Example = () => {
+  return (
+    <div className={styles.title}>Example</div>
+  )
+}
+
+export default Example
