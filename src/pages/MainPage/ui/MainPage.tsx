c5cace85c1b8b601d1e4061611c7c1a174efdc5a
esolodovnik <elizavetasolodovnik12@mail.ru>
2023-03-21 14:32:48 +0300

    add babel, add extract plugin

---
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next';
+
+const MainPage = () => {
+  const { t, i18n } = useTranslation('main');
+  
+  return (
+    <div>{t('Главная страница')}</div>
+  )
+}
+
+export default MainPage
---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,32 @@
+import React, { ButtonHTMLAttributes, FC } from 'react'
+import { classNames } from 'shared/lib/classNames/classNames'
+import cls from './Button.module.scss'
+
+export enum ThemeButton {
+    CLEAR = 'clear',
+
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+    className?: string;
+    theme: ThemeButton
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+    const {
+        className,
+        theme,
+        children, 
+        ...otherProps
+    } = props
+
+  return (
+    <button 
+        {...otherProps}
+        className={classNames(cls.Button, {}, [className, cls[theme]])}
+    >
+        {children}
+    </button>
+  )
+};
+
